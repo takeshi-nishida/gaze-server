7c1d17100a6ac17dfecad046982d5a44c2914373
tnishida <tnishida@localhost>
2013-11-08 16:16:16 +0900

    Added calibration and mouse emulation mode.

---
+++ MouseHookAPI.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace GazeServer
+{
+    class MouseHookAPI
+    {
+        public delegate IntPtr HookProcedureDelegate(int nCode, IntPtr wParam, IntPtr lParam);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern IntPtr SetWindowsHookEx(int idHook, HookProcedureDelegate lpfn, IntPtr hInstance, int threadId);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern bool UnhookWindowsHookEx(IntPtr idHook);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern IntPtr CallNextHookEx(IntPtr idHook, int nCode, IntPtr wParam, IntPtr lParam);
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr GetModuleHandle(String lpModuleName);
+    }
+    public static class MouseHookInfo
+    {
+        public static IntPtr hHook = IntPtr.Zero;
+        public const int WH_MOUSE_LL = 14;
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int x;
+            public int y;
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        public class MouseHookStruct
+        {
+            public POINT pt;
+            public uint mouseData;
+            public uint flags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+        public const int WM_LBUTTONDOWN = 0x201;
+        public const int WM_LBUTTONUP = 0x202;
+        public const int WM_RBUTTONDOWN = 0x204;
+        public const int WM_RBUTTONUP = 0x205;
+        public const int WM_MBUTTONDOWN = 0x207;
+        public const int WM_MBUTTONUP = 0x208;
+        public const int WM_MBUTTONDBLCLK = 0x209;
+        public const int WM_MOUSEMOVE = 0x200;
+        public const int WM_MOUSEWHEEL = 0x20A;
+    }
+}
